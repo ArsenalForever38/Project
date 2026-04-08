9ee478fd9dedb724bf82e3a0553117bf30000e19
Ivan <Ivan@DESKTOP-O5RRP0D>
2026-04-09 02:45:00 +0800

    Обновил

--- Project.cpp
+++ Project.cpp
@@ -96,7 +96,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1024, 1024, "Rotating Square with Camera", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1024, 1024, "Lab4 - Camera", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
